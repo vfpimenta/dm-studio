428a43423462ec65024705d4557a6f02a1b551af
vfpimenta <vfpimenta.93@gmail.com>
2017-09-29 11:42:03 -0300

    Multiple rows behaviour.

--- app/src/main/java/vfpimenta/dungeonmasterstudio/fragments/EncounterCalculatorFragment.java
+++ app/src/main/java/vfpimenta/dungeonmasterstudio/fragments/EncounterCalculatorFragment.java
@@ -1,20 +1,23 @@
 package vfpimenta.dungeonmasterstudio.fragments;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import vfpimenta.dungeonmasterstudio.R;
 
-public class EncounterCalculatorFragment extends Fragment {
+public class EncounterCalculatorFragment extends Fragment implements View.OnClickListener {
 
     public static final String ARG_PAGE = "ARG_PAGE";
 
     private int mPage;
+    private View mView;
 
     public static EncounterCalculatorFragment newInstance(int page) {
         Bundle args = new Bundle();
@@ -33,13 +36,42 @@ public class EncounterCalculatorFragment extends Fragment {
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_encounter_calculator, container, false);
-        RelativeLayout playerContainer = view.findViewById(R.id.player_container);
+
+        LinearLayout playerContainer = view.findViewById(R.id.player_container);
         View playerView = inflater.inflate(R.layout.view_player_layout, null);
         playerContainer.addView(playerView);
-        RelativeLayout enemyContainer = view.findViewById(R.id.enemy_container);
+        LinearLayout enemyContainer = view.findViewById(R.id.enemy_container);
         View enemyView = inflater.inflate(R.layout.view_enemy_layout, null);
         enemyContainer.addView(enemyView);
+
+        ImageButton addPlayer = view.findViewById(R.id.add_player);
+        addPlayer.setOnClickListener(this);
+        ImageButton addEnemy = view.findViewById(R.id.add_enemy);
+        addEnemy.setOnClickListener(this);
+
+        setView(view);
         return view;
     }
 
+    private void setView(View v){
+        this.mView = v;
+    }
+
+    public void onClick(View v){
+        switch (v.getId()) {
+            case  R.id.add_player: {
+                LinearLayout playerContainer = mView.findViewById(R.id.player_container);
+                View playerView = LayoutInflater.from(mView.getContext()).inflate(R.layout.view_player_layout, null);
+                playerContainer.addView(playerView);
+                break;
+            }
+
+            case R.id.add_enemy: {
+                LinearLayout enemyContainer = mView.findViewById(R.id.enemy_container);
+                View enemyView = LayoutInflater.from(mView.getContext()).inflate(R.layout.view_enemy_layout, null);
+                enemyContainer.addView(enemyView);
+                break;
+            }
+        }
+    }
 }
